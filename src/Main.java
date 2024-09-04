72e6415abea88bab3855fc8718a425c1f1e0bad8
JustSG <szymixxxyt@gmail.com>
2024-09-04 15:43:27 +0200

    dodatkowy komunikat

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,18 @@ public class Main {
     //Nazwa klasy muszą się zaczynać z dużej litery
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Dzień Dobry Polsko!");
     }
 }
 /*git init - puste reprezytorium
+  git config --global - żeby zapamiętało konto
   git config --local user.name "JustSG" - login
   git config --local user.email "szymixxxyt@gmail.com" - email
+  git add . - dodaj wszystkie pliki do reprezytorium
+  git commit -m "pusty projekt" - tworzy migawke o nazwie pusty projekt
+  git log - aby sprawdzić commita
+  git remote add origin https://github.com/JustSG/Zgadywanka.git -
+  git push -u origin master -
+
+  menedżer poświadczeń
  */

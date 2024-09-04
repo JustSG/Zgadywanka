5fbc347f0dac8439513dacfac8091ba122bf116a
JustSG <szymixxxyt@gmail.com>
2024-09-04 15:20:57 +0200

    pusty projekt

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    //Nazwa pliku i nazwa klasy muszą być takie same
+    //Nazwa klasy muszą się zaczynać z dużej litery
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+/*git init - puste reprezytorium
+  git config --local user.name "JustSG" - login
+  git config --local user.email "szymixxxyt@gmail.com" - email
+ */
